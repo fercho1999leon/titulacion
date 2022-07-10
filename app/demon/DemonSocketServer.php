b2bef3553315b5842e4428946c36442ee3903fb1
fercho1999leon <fercho1999_w@hotmail.com>
2022-07-10 09:40:00 -0500

    some message

---
+++ app/demon/DemonSocketServer.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Events\ListenerLineElectricEvent;
+
+$socket = null;
+while (true) {
+	try {
+		$socket = stream_socket_server("tcp://192.168.2.2:5478", $errno, $errstr);
+		if (!$socket) {
+			echo "$errstr ($errno)<br />\n";
+		} else {
+			while ($conn = stream_socket_accept($socket)) {
+				fwrite($conn, "\nBienvenido al Servidor De Prueba de PHP. \n");
+				$datos = stream_get_contents($conn);
+				if (isJson($datos)) {
+					$datos = json_decode($datos);
+					$evento = new ListenerLineElectricEvent($datos['v1'],$datos['v1'],0,0);
+				}
+				echo $datos;
+				fclose($conn);
+			}
+			fclose($socket);
+		}
+	} catch (Exception $e) {
+		if (!$socket) fclose($socket);
+	}
+}
+
+function isJson($string)
+{
+	return ((is_string($string) && (is_object(json_decode($string)) || is_array(json_decode($string))))) ? true : false;
+}
