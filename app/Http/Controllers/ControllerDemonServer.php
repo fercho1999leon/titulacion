b03e5f4051b9e04653818e7969ab6ca078c00c82
fercho1999leon <fercho1999_w@hotmail.com>
2022-07-10 10:37:41 -0500

    cambios

---
+++ app/Http/Controllers/ControllerDemonServer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\ListenerLineElectricEvent;
+
+class ControllerDemonServer extends Controller
+{
+    public function start()
+    {
+        $socket = null;
+        while (true) {
+            try {
+                $socket = stream_socket_server("tcp://192.168.2.13:5478", $errno, $errstr);
+                if (!$socket) {
+                    echo "$errstr ($errno)<br />\n";
+                } else {
+                    while ($conn = stream_socket_accept($socket)) {
+                        fwrite($conn, "\nBienvenido al Servidor De Prueba de PHP. \n");
+                        $datos = stream_get_contents($conn);
+                        echo $datos;
+                        if ($this->isJson($datos)) {
+                            $datos = json_decode($datos);
+                            //;
+                            $evento = new ListenerLineElectricEvent(10, 20, 0, 0);
+                        }
+                        //echo $datos;
+                        fclose($conn);
+                    }
+                    fclose($socket);
+                }
+            } catch (\Exception $e) {
+                if (!$socket) fclose($socket);
+            }
+        }
+    }
+    private function isJson($string)
+    {
+        return ((is_string($string) && (is_object(json_decode($string)) || is_array(json_decode($string))))) ? true : false;
+    }
+}
